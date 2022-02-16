ad3e32105ccb2f8a7dd250c613f6615f2fdc8691
Jean Phe <jean.phe.fr@gmail.com>
2022-02-16 15:15:54 +0100

    fix(eslint): update eslint config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <h1 className="text-3xl text-red-400 font-bold underline">
-                Hello world!
-            </h1>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-red-400 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
 }
 
 export default App;
