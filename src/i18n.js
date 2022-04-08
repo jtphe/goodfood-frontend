1ea843929016b60667696453b6360eeebb9dd290
dc4665 <daihiep.cao@gmail.com>
2022-04-08 10:36:28 +0200

    feat: add navigator detection

--- src/i18n.js
+++ src/i18n.js
@@ -15,10 +15,10 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
-    fallbackLng: 'en',
+    fallbackLng: 'fr',
     detection: {
-      order: ['queryString', 'cookie'],
-      cache: ['cookier'],
+      order: ['queryString', 'navigator', 'cookie'],
+      cache: ['cookie'],
     },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
