0c1c9965a64c73c9661586defc0a1c24b16f163d
dc4665 <daihiep.cao@gmail.com>
2022-05-29 10:42:55 +0200

    feat: add table and translation

--- src/components/Parameters.js
+++ src/components/Parameters.js
@@ -12,15 +12,15 @@ function Parameters() {
     <div>
       <h1>{t('parametersPage.title')}</h1>
       <div className="flex flex-col justify-between mt-6 p-5">
-        <div className="flex justify-around mb-5">
+        <div className="flex mb-5">
           <button
-            className="bg-slate-300 w-2/4 py-2 px-5 mr-1 rounded-lg hover:bg-red-400 hover:text-white"
+            className="bg-slate-300 py-2 px-5 mr-1 rounded-lg hover:bg-red-400 hover:text-white"
             onClick={() => changeLanguage('en')}
           >
             {t('languages.english')}
           </button>
           <button
-            className="bg-slate-300 w-2/4 py-2 px-5 rounded-lg hover:bg-red-400 hover:text-white"
+            className="ml-4 bg-slate-300 py-2 px-5 rounded-lg hover:bg-red-400 hover:text-white"
             onClick={() => changeLanguage('fr')}
           >
             {t('languages.french')}
