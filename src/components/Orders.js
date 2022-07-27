1026c71bde276d0232f600373e443ec1666e9f91
dai <56730311+dc4665@users.noreply.github.com>
2022-07-27 16:03:29 +0200

    feat: add first view for order (#13)
    * feat: add first view for order
    
    * feat: refactored Button component
    
    * feat: remove unecessary params

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,12 +1,61 @@
 import React from 'react';
+import Button from './utilities/Button';
 import { useTranslation } from 'react-i18next';
 
 function Orders() {
   const { t } = useTranslation();
 
   return (
-    <div className="flex">
-      <h1>{t('ordersPage.title')}</h1>
+    <div>
+      <h1 className="text-4xl text-goodFoodRed-500 font-bold">
+        {t('ordersPage.title')}
+      </h1>
+      <p className="py-6 text-goodFoodMustard-500">
+        {t('ordersPage.description')}
+      </p>
+
+      <div>
+        <table className="border-collapse text-gray-600 table-fixed md:table-auto">
+          <thead>
+            <tr className="border-b-2 border-goodfFoodGrey-500">
+              <th className="w-1/6 p-2">{t('ordersPage.orderNumber')}</th>
+              <th className="w-2/6 p-2">{t('ordersPage.adress')}</th>
+              <th className="w-1/6 p-2">{t('ordersPage.order')}</th>
+              <th className="w-1/6 p-2">{t('ordersPage.price')}</th>
+              <th className="w-1/6 p-2">{t('ordersPage.status')}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="text-left">
+              <td className="py-2 px-4">#1</td>
+              <td className="py-2 px-4">12 rue du puit 67000, Strasbourg</td>
+              <td className="py-2 px-4">Menu Burger</td>
+              <td className="py-2 px-4 text-center">12$</td>
+              <td className="py-2 px-4 text-center">
+                <Button type="processing" />
+              </td>
+            </tr>
+            <tr className="text-left">
+              <td className="py-2 px-4">#1</td>
+              <td className="py-2 px-4">12 rue du puit 67000, Strasbourg</td>
+              <td className="py-2 px-4">Menu Burger</td>
+              <td className="py-2 px-4 text-center">12$</td>
+              <td className="py-2 px-4 text-center">
+                <Button type="delivering" />
+              </td>
+            </tr>
+            <tr className="text-left">
+              <td className="py-2 px-4">#1</td>
+              <td className="py-2 px-4">12 rue du puit 67000, Strasbourg</td>
+              <td className="py-2 px-4">Menu Burger</td>
+              <td className="py-2 px-4 text-center">12$</td>
+              <td className="py-2 px-4 text-center">
+                <Button type="delivered" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/components/utilities/Button.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+
+function Button({ type }) {
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const { t } = useTranslation();
+
+  const setBtnColor = () => {
+    if (type === 'processing') {
+      return 'bg-cyan-500 hover:bg-cyan-700 text-white';
+    } else if (type === 'delivering') {
+      return 'bg-yellow-500 hover:bg-yellow-700 text-white';
+    } else if (type === 'delivered') {
+      return 'bg-green-500 hover:bg-green-700 text-white';
+    }
+  };
+
+  const setBtnText = () => {
+    return capitalizeFirstLetter(t(`utilities.button.${type}`));
+  };
+
+  return (
+    <button
+      className={`${setBtnColor()} font-bold py-2 px-4 rounded-full w-40`}
+    >
+      {setBtnText()}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  type: propTypes.string.isRequired
+};
+
+export default Button;
